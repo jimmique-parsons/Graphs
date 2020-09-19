8c0dc3c8d552b2db21bb8d21883427692854bb2c
Jimmique <jimmique88@gmail.com>
2020-09-18 17:16:23 -0700

    added vertex and edge methods

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -14,13 +14,15 @@ class Graph:
         Add a vertex to the graph.
         """
         pass  # TODO
+        self.vertices[vertex_id] = set()
 
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
         pass  # TODO
-
+        self.vertices[v1].add(v2)
+        
     def get_neighbors(self, vertex_id):
         """
         Get all neighbors (edges) of a vertex.
